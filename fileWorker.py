72e29fb262277b98233533e50dbbbf4c4757f35b
Romass <haker228708@gmail.com>
2023-11-11 15:41:19 +0600

    WF fix0.1

--- fileWorker.py
+++ fileWorker.py
@@ -1,10 +1,14 @@
+import os.path
+from os import path
 import character.py
 
 class FileWorker():
 
     def _init_(self, char):
         self.char = char
-        f = open(char.name, '+')
+        if path.exists(char.name + ".dnd"):
+            f = open(char.name + ".dnd", '+')
     
     def write_to_file():
-        
+        charDict = {"title" : "CHARACTER", "data" : self.char.scores}
+        dmpdChar = json.dump(charDict, ensure_ascii=False)
