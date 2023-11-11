542b4e9c8d81a015eda078f1c221646f579b2f9c
Romass <haker228708@gmail.com>
2023-11-12 03:19:46 +0600

    da ya ne znay

---
+++ main.py
@@ -0,0 +1,20 @@
+from character import Charecter
+from fileWorker import FileWorker
+from charactercreate import CharacterCreatorApp
+
+char
+fw = FileWorker()
+
+charList = fw.getCharList()
+
+if __name__ == '__main__':
+    CharacterCreatorApp(charList).run()
+
+while flag:
+# Если пользователь выбирает готового персонажа из charList, вызываешь read_from_file
+# Если хочет создать персонажа, делаешь char = Charecter('имя','РАССА','КЛАСС') и char.generate()
+# Сохранение персонажа на write_to_file, но нужно сделать fw = FileWorker(char)
+# 
+# 
+#
+    print("Pizdec")
