931bcbe3ff0f08d5d3b7599ceb9efc25ade8c716
Max <pyccto@gmail.com>
2023-11-11 11:54:28 +0600

    FIX

--- character.py
+++ character.py
@@ -10,15 +10,17 @@ class Charecter():
         self.hp = 0
         self.cd = 0
         self.masterbonus = 2
-        self.bests = dict(воин = [3,2,2,-1,0,1], паладин = [1,0,2,-1,3,2], лучник = [2,3,1,-1,2,0], плут = [-1,3,0,2,1,2])
-        
+        self.startscor = dict(воин = [3,2,2,-1,0,1], паладин =[1,0,2,-1,3,2], лучник = [2,3,1,-1,2,0], плут = [-1,3,0,2,1,2])
+        self.bestscor = dict(воин = [0,2], паладин = [4,5], лучник = [0,1], плут = [1,3])
+
     def mainscorregul(self):
-        self.mainscor = self.bests[self.clas]
+        self.mainscor = self.startscor[self.clas]
         return self.mainscor
 
     def selfthrowgen(self):
         self.selfthrow = self.mainscor
-        self.selfthrow[0] = self.selfthrow[0] + self.masterbonus
+        self.selfthrow[self.bestscor[self.clas][0]] = self.selfthrow[0] + self.masterbonus
+        self.selfthrow[self.bestscor[self.clas][1]] = self.selfthrow[0] + self.masterbonus
         return self.selfthrow
 
 lol = Charecter(name='Roma', race='Эльф', clas='воин')
