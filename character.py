bae17eadb5a3032178ae6f96c08478879be8b434
Max <pyccto@gmail.com>
2023-11-11 11:19:28 +0600

    FIX

--- character.py
+++ character.py
@@ -11,22 +11,15 @@ class Charecter():
         self.cd = 0
         self.masterbonus = 2
         self.bests = dict(воин = [3,2,2,-1,0,1], паладин = [1,0,2,-1,3,2], лучник = [2,3,1,-1,2,0], плут = [-1,3,0,2,1,2])
-
-    def rol(self):
-        for i in range(6):
-            if (self.clas) == self.numclas[i]:
-                self.numclas = i
-                return self.numclas
         
     def mainscorregul(self):
-        self.mainscor = self.bests[self.numclas]
+        self.mainscor = self.bests[self.clas]
         return self.mainscor
 
     def selfthrowgen(self):
-        for i in range(6):
-            self.selfthrow[i] = self.mainscor[i]
+        self.selfthrow = self.mainscor
+        self.selfthrow[0] = self.selfthrow[0] + self.masterbonus
         return self.selfthrow
-    
 
 lol = Charecter(name='Roma', race='Эльф', clas='воин')
 print(lol.mainscorregul())
